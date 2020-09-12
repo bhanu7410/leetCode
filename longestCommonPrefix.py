392489b16b591ce363542abdbfc5d1dc391d904e
Bhanu <221910301012@gitam.in>
2020-09-13 02:21:51 +0530

    hee

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,13 +1,12 @@
-class Solution:
-    def longestCommonPrefix(inn):        
-        if len(inn)==0 :
-            return ""
-        shortest = min(inn,key=len)
-        for i, ch in enumerate(shortest):
-            for other in inn:
-                if other[i] != ch:
-                    return shortest[:i]
-        return shortest
+def longestCommonPrefix(inn):        
+    if len(inn)==0 :
+        return ""
+    shortest = min(inn,key=len)
+    for i, ch in enumerate(shortest):
+        for other in inn:
+            if other[i] != ch:
+                return shortest[:i]
+    return shortest
 
 strs=["future","futel","futanari"]
-print(Solution.longestCommonPrefix(strs))
+print(longestCommonPrefix(strs))
