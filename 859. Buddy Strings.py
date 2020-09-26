b5ca74452aaf232d8f5246ed79a1164aa018c89e
Bhanu Prabhth <70201186+bhanu7410@users.noreply.github.com>
2020-09-26 16:56:00 +0530

    to

---
+++ 859. Buddy Strings.py
@@ -0,0 +1,19 @@
+class Solution:
+    def buddyStrings(self, A: str, B: str) -> bool:
+        res = False
+        if len(A) != len(B):
+            return False
+        if A == B:
+            if len(A) - len(set(A)) != 0:
+                res = True
+        else:
+            n = 0
+            arrA, arrB = [], []
+            for i in range(len(A)):
+                if A[i] != B[i]:
+                    n += 1
+                    arrA.append(A[i])
+                    arrB.append(B[i])
+            if n <=2 and set(arrA) == set(arrB):
+                res = True
+        return res
